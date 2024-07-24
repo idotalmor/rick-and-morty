956888aec5e82ecc635abffd8a6198a342cb714e
ido talmor <talmorid@gmail.com>
2024-07-24 23:07:50 +0300

    start working on e2e

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,18 +1,28 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:get_it/get_it.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart' show ProviderScope;
+
+import 'package:indieflow/core/index.dart' show setupLocator;
 import 'package:indieflow/main.dart';
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() {
+    setupLocator(); // Setup GetIt
+  });
+
+  tearDownAll(() {
+    GetIt.I.reset(); // Cleanup GetIt after all tests
+  });
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ProviderScope(child:MyApp()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
