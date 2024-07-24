e60071a44db5d556bb98427112afd34f9eda9218
ido talmor <talmorid@gmail.com>
2024-07-24 19:26:56 +0300

    added retry from error state

--- lib/features/character_list/widgets/error_state.dart
+++ lib/features/character_list/widgets/error_state.dart
@@ -2,11 +2,25 @@ import 'package:flutter/material.dart';
 
 class ErrorState extends StatelessWidget {
   final String message;
+  final VoidCallback onPress;
 
-  ErrorState({required this.message});
+  ErrorState({required this.message, required this.onPress});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Error: $message'));
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Center(child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text('Error: $message'),
+          SizedBox(height: 20,),
+          ElevatedButton(
+            onPressed: onPress,
+            child: Text('Retry'),
+          )
+        ],
+      )),
+    );
   }
 }
